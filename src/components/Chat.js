ea2e7703f23621af8cd590a7bad0b15803baa6b3
Oğuz GÜL <ogzgl06@gmail.com>
2021-09-07 13:24:26 +0300

    Build complated ChatFooter side for Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,13 +4,21 @@ import "./Chat.css";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("Youu Type >>>>>", input);
+  };
 
   return (
     <div className="chat">
@@ -38,15 +46,28 @@ function Chat() {
       </div>
 
       <div className="chat__body">
-        <p className={`chat__message ${true && 'chat__reciever'}`}>
+        <p className={`chat__message ${true && "chat__reciever"}`}>
           <span className="chat__name">Oguz GUL</span>
           Hey Guys
-
           <span className="chat__timestamp">3:25pm</span>
         </p>
       </div>
 
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
